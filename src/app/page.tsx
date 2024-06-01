c58cc51c585c0a127bf2bfd3bd04a457761d97f2
kan <ninomiya@kan.run>
2024-06-01 21:31:16 +0900

    表示する内容を調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,15 @@ export default function Home() {
               <span className={'ml-2'}>(18)</span>
             </h2>
           </div>
-          <p className={'text-sm'}>
+          <div>
+            <p className={'text-xs leading-5 p-4 bg-gray-100 rounded-md'}>
             幼少期からPCを触り始め、小学校低学年よりプログラミングを開始、中学生で初めてWebサービスのリリースを経験。
             <br />
             新規サービス開発を得意としており、マーケティングから実装に至るまで幅広い分野の知見があります。
             <br />
             AIを活用したサービス開発に早期から着手し、2022年10月にLINEで使用可能なAIチャットボット「Lina（現在の友達数は1万人以上）」を開発し、事業譲渡を行ったのち株式会社FP16を共同創業。
           </p>
+          </div>
         </div>
       </div>
     </main>
