bbceb1d278e2a22064731e22135ce268065719a2
kan ninomiya <79746996+nnmykn@users.noreply.github.com>
2024-06-18 20:35:40 -0700

    にくその追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,46 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import localFont from 'next/font/local'
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from "next/font/local";
 
 const lineSeedFont = localFont({
   src: [
     {
-      path: '../fonts/line-seed/LINESeedJP_OTF_Rg.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "../fonts/line-seed/LINESeedJP_OTF_Rg.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../fonts/line-seed/LINESeedJP_OTF_Th.woff2',
-      weight: '100',
-      style: 'normal',
+      path: "../fonts/line-seed/LINESeedJP_OTF_Th.woff2",
+      weight: "100",
+      style: "normal",
     },
     {
-      path: '../fonts/line-seed/LINESeedJP_OTF_Bd.woff2',
-      weight: '700',
-      style: 'normal',
+      path: "../fonts/line-seed/LINESeedJP_OTF_Bd.woff2",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../fonts/line-seed/LINESeedJP_OTF_Eb.woff2',
-      weight: '800',
-      style: 'normal',
+      path: "../fonts/line-seed/LINESeedJP_OTF_Eb.woff2",
+      weight: "800",
+      style: "normal",
     },
   ],
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Kan Ninomiya | 二宮 貫',
+  title: "Kan Ninomiya | 二宮 貫 | にくそ(nixo)",
   description:
-    '18歳です。広島にいます。写真と音とパソコンが好きです。 株式会社FP16で代表取締役社長兼COOやってます。 ITとAIの会社です。バクノビtoCサービスを作ってます。',
-}
+    "18歳です。広島にいます。写真と音とパソコンが好きです。 株式会社FP16で代表取締役社長兼COOやってます。 ITとAIの会社です。",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="ja" className={`${lineSeedFont.className}`}>
-      <body className={''}>{children}</body>
+      <body className={""}>{children}</body>
     </html>
-  )
+  );
 }
